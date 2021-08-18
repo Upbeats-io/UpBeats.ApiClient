cf96db3fbf37849e6b826e880c2108cbe3719f4c
Jon Menzies-Smith <github@menzies-smith.co.uk>
2021-08-18 22:03:39 +0100

    Add Solution

---
+++ src/UpBeats.ApiClient/ApiException.cs
@@ -0,0 +1,19 @@
+﻿namespace UpBeats.ApiClient
+{
+    using System;
+    using System.Net;
+
+    public class ApiException : Exception
+    {
+        public ApiException(HttpStatusCode statusCode, string responseContent) 
+            : base($"Request failed with status '{statusCode:int}'")
+        {
+            this.StatusCode = statusCode;
+            this.Response = responseContent;
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        public string Response { get; set; }
+    }
+}
