433b03780a4a5ad77c37bd7fc7b52654e5edd280
Jon Menzies-Smith <github@menzies-smith.co.uk>
2021-08-18 22:03:39 +0100

    Add Solution

---
+++ src/UpBeats.ApiClient/Model/HealthCheckResultProbeStatusV1.cs
@@ -0,0 +1,47 @@
+﻿namespace UpBeats.ApiClient.Model
+{
+    using System.Collections.Generic;
+
+    public class HealthCheckResultProbeStatusV1
+    {
+        /// <summary>
+        /// Identifier to differentiate probes within the service
+        /// </summary>
+        public string ProbeName { get; set; }
+
+        /// <summary>
+        /// The underlying type used to perform the test (optional)
+        /// </summary>
+        public string ProbeType { get; set; }
+
+        /// <summary>
+        /// (optional)
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// The duration elapsed to perform the probe test (optional)
+        /// </summary>
+        public int? Duration { get; set; }
+
+        /// <summary>
+        /// The status of the probe
+        /// </summary>
+        public SeverityLevel Level { get; set; }
+
+        /// <summary>
+        /// Short summary of the probe status (optional)
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Detailed information that can be used to diagnose failures. (optional)
+        /// </summary>
+        public string Details { get; set; }
+
+        /// <summary>
+        /// Additional key/value information to provide extra context.
+        /// </summary>
+        public Dictionary<string, string> Metadata { get; set; }
+    }
+}
---
+++ src/UpBeats.ApiClient/UpBeatsApiClient.cs
@@ -0,0 +1,39 @@
+﻿namespace UpBeats.ApiClient
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using UpBeats.ApiClient.Model;
+
+    public partial class UpBeatsApiClient : IUpBeatsApiClient
+    {
+        public UpBeatsApiClient(HttpClient client)
+        {
+            this.Client = client;
+        }
+
+        public UpBeatsApiClient(string baseUrl = "https://api.upbeats.io")
+        {
+            this.Client = new HttpClient()
+            {
+                BaseAddress = new Uri(baseUrl),
+            };
+        }
+
+        public async Task<SubmitHealthCheckResponseV1> SubmitHealthCheckAsync(SubmitHealthCheckRequestV1 healthCheck, string apiKey)
+        {
+            var headers = this.BuildHeaders(apiKey);
+
+            return await this.PostAsync<SubmitHealthCheckRequestV1, SubmitHealthCheckResponseV1>(new Uri("health-checks", UriKind.Relative), healthCheck, headers);
+        }
+
+        private Dictionary<string, string> BuildHeaders(string apiKey)
+        {
+            return new Dictionary<string, string>
+            {
+                {"X-Api-Key", apiKey}
+            };
+        }
+    }
+}
